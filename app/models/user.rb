6194326249574a099c5e2309924433d86d42edf6
Charles McClain <charles.mcclain@hgs.hiddengeniusproject.org>
2020-01-09 23:17:05 -0500

    add changes to the user model to enforce format and email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	validates :name, presence: true, length: {maximum: 60, minimum: 6}
+	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length:{maximum: 255}, format:{with: EMAIL_REGEX},
+	uniqueness: {case_sensitive: false}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user = User.new(name: "test user", email: "user@test.com")
+	end
+
+  test "should be valid" do
+		assert @user.valid?
+	end
+
+	test "should have name" do
+		@user.name = "       "
+		assert_not @user.valid?
+	end
+
+	test "should have email" do
+		@user.email = "       "
+		assert_not @user.valid?
+	end
+
+	test "name should be right length" do
+		@user.name = "a"*61
+		assert_not @user.valid?
+		@user.name = "a"*5
+		assert_not @user.valid?
+	end
+
+	test "email shouldn't be too long" do
+		@user.email = "a" * 244 + "@example.com"
+	end
+
+	test "email should have valid format" do
+		invalid = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+		invalid.each do |address|
+			@user.email = address
+			assert_not @user.valid? "#{address} should be invalid"
+		end
+	end
+
+	test "users should have unique emails" do
+		duplicate = @user.dup
+		@user.save
+		duplicate.email = @user.email.upcase
+		assert_not duplicate.valid?
+	end
 end
