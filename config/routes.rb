171f6c476b78553aa2106730da7d16797c88c1c3
Charles McClain <charles.mcclain@hgs.hiddengeniusproject.org>
2020-01-14 06:25:22 -0500

    add users resource pathing to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'basic_pages/home'
   get 'basic_pages/about'
   get 'basic_pages/help'
+	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
