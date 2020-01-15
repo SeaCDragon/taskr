91f6499c3ef8bf0e4235e5017729ff28e8dbb03d
Charles McClain <charles.mcclain@hgs.hiddengeniusproject.org>
2020-01-15 13:56:57 -0500

    add show function and view to the users controller. label routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-	root 'basic_pages#home'
-  get 'basic_pages/home'
-  get 'basic_pages/about'
-  get 'basic_pages/help'
+  get "/signup", to: 'users#new' #signup_path
+	root 'basic_pages#home' #root_path
+  get 'basic_pages/home' #basic_pages_home_url
+  get 'basic_pages/about' #basic_pages_about_url
+  get 'basic_pages/help' #basic_pages_help_url
 	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
