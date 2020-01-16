511ca0ac8cf681ec00b53c2492f63e957c2c6c9c
Charles McClain <charles.mcclain@hgs.hiddengeniusproject.org>
2020-01-15 23:28:33 -0500

    clean up merge problems

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new' #signup_path
 	root 'basic_pages#home' #root_path
-  get '/about', to: 'basic_pages/about' #about_path
-  get '/help', to: 'basic_pages/help' #help_path
+  get '/about', to: 'basic_pages#about' #about_path
+  get '/help', to: 'basic_pages#help' #help_path
 	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
