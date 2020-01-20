c4c63224783bad8599da5125ee8ceac160dccc19
Charles McClain <charles.mcclain@hgs.hiddengeniusproject.org>
2020-01-20 07:00:35 -0500

    finish session#create, relevant tests, session helper functions, include session helper in application controller for app-wide access

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+	#logs in as the user passed as the arguement
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	def current_user
+		if session[:user_id]
+			@current_user ||= User.find_by(id: session[:user_id])
+		end
+	end
 end
