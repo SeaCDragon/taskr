d723917ea7f83f832d88555206fe5a65de20741a
Charles McClain <charles.mcclain@hgs.hiddengeniusproject.org>
2020-01-20 05:29:03 -0500

    add login/logout named routes, comments, cleaner format to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new' #signup_path
+	# Root
 	root 'basic_pages#home' #root_path
+	# Basic Pages
   get '/about', to: 'basic_pages#about' #about_path
-  get '/help', to: 'basic_pages#help' #help_path
+  get '/help',  to: 'basic_pages#help' #help_path
+
+	# Users
+	get  '/signup', to: 'users#new' #signup_path
 	post '/signup', to: 'users#create' #signup_path
-	resources :users
+	resources :users #Controller Methods made available: show, edit, index, update, destroy
+									 #RESTful Equivalents respectively:  GET   GET   GET    PATCH   DELETE
+	
+	# Sessions (Login/Logout)
+	get    '/login',  to: 'sessions#new'
+	post   '/login',  to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
