a1fd302d5cd4cc927b76028a9538b12ee235d5be
Charles McClain <charles.mcclain@hgs.hiddengeniusproject.org>
2020-01-27 05:07:08 -0500

    big boy changes: new integration test for editing user info, user model now allows blank passwords but only when editing not creating, controller now keeps people from editing other users info, and the controller test tests for that functionality. also adds new fixture for testing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,39 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+	def setup
+		@user1 = users(:Charles)
+		@user2 = users(:Ellis)
+	end
+
   test "should get new" do
     get signup_path
     assert_response :success
   end
 
+	test "should redirect edit when logged out" do
+		get edit_user_path(@user1)
+		assert_not flash.empty?
+		assert_redirected_to login_url
+	end
+
+	test "should redirect update when logged out" do
+		patch user_path(@user1), params:{user:{name: @user1.name, email: @user1.email}}
+		assert_not flash.empty?
+		assert_redirected_to login_url
+	end
+
+	test "should redirect edit when logged in as wrong user" do
+		testv_log_in(@user2)
+		get edit_user_path(@user1)
+		assert flash.empty?
+		assert_redirected_to root_url
+	end
+
+	test "should redirect update when logged in as wrong user" do
+		testv_log_in(@user2)
+		patch user_path(@user1), params:{user:{name: @user1.name, email: @user1.email}}
+		assert flash.empty?
+		assert_redirected_to root_url
+	end
 end
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+	def setup
+		@user = users(:Charles)
+	end
+
+  test "unsuccessful edit" do
+		testv_log_in(@user)
+  	get edit_user_path(@user)
+		assert_template 'users/edit'
+		patch user_path(@user), params:{user:{name: "", email: "this@invalid", password: "foo", password_confirmation: "bar"}}
+		assert_template 'users/edit'
+		assert_select 'div.alert', 'The form contains 5 errors'
+  end
+
+	test "successful edit with friendly forwarding" do
+		get edit_user_path(@user)
+		testv_log_in(@user)
+		assert_redirected_to edit_user_url(@user)
+		name = "Foo Bar"
+		email = "foo@bar.com"
+		patch user_path(@user), params: { user: {name: name, email: email, password: "", password_confirmation: ""}}
+		assert_not flash.empty?
+		assert_redirected_to @user
+		@user.reload
+		assert_equal name, @user.name
+		assert_equal email, @user.email
+	end
+end
