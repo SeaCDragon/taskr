cdaf7b2e4b255d404d9980eacb95413c3e1c6a5c
Charles McClain <charles.mcclain@hgs.hiddengeniusproject.org>
2020-01-28 12:58:06 -0500

    add fixtures and tests for admin property of users. as of this commit, they all fail

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -23,6 +23,34 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 		assert_redirected_to login_url
 	end
 
+	test "should prevent creating an admin from the web" do
+		testv_log_in(@user2)
+		assert_not @user2.admin?
+		patch user_path(@user2), params:{user:{password: 'password', password_confirmation: 'password', admin: true}}
+		assert_not @user2.reload.admin?
+	end
+
+	test "should redirect deletion to login when not logged in" do
+		assert_no_difference 'User.count' do
+			delete user_path(@user1)
+		end
+		assert_redirected_to login_url
+	end
+
+	test "should redirect deletion to root when user is not admin" do
+		testv_log_in(@user2)
+		assert_no_difference 'User.count' do
+			delete user_path(@user1)
+		end
+		assert_redirected_to root_url
+	end
+
+	test "index is for admin eyes only" do
+		testv_log_in(@user2)
+		get users_path
+		assert_redirected_to root_url
+	end
+
 	test "should redirect edit when logged in as wrong user" do
 		testv_log_in(@user2)
 		get edit_user_path(@user1)
