149f0177bf5c79a50953f9efa6cd273d26b1291f
Charles McClain <charles.mcclain@hgs.hiddengeniusproject.org>
2020-01-31 12:34:10 -0500

    add model tests for project, begin user/micropost relationship

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@user = users(:Charles)
+    # This code is idiomatically wrong
+    @project = @user.projects.build(title: "test title")
+	end
+
+	test "sould be valid" do
+		assert @project.valid?
+	end
+
+	test "user id should be present" do
+		@project.user_id = nil
+		assert_not @project.valid?
+	end
+
+	test "title should be present" do
+		@project.title = "         "
+		assert_not @project.valid?
+	end
+
+	test "title canot be too long" do
+		@project.title = "a"*101
+		assert_not @project.valid?
+	end
 end
