062cccff8224a344bcc0430f1e610950774e2096
Charles McClain <charles.mcclain@hgs.hiddengeniusproject.org>
2020-02-01 03:12:14 -0500

    project routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,8 @@ Rails.application.routes.draw do
 
 	# Password Reset
 	resources :password_resets, only: [:new, :edit, :create, :update]
+
+	#Projects
+	resources :projects, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
