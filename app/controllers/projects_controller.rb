df06b6338a93410ab198b3ef57746f2761f03860
Charles McClain <charles.mcclain@hgs.hiddengeniusproject.org>
2020-02-01 20:16:16 -0500

    controller method adjustments

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,6 +21,7 @@ class ProjectsController < ApplicationController
 	def show
 		@project = Project.find(params[:id])
 		@tasks = @project.tasks.paginate(page: params[:page])
+		@task = @project.tasks.build
 	end
 
 	private
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,21 @@
 class TasksController < ApplicationController
+	def create
+		@project = Project.find(params[:project_id])
+		@task = @project.tasks.build(task_params)
+		if @task.save
+			redirect_to @project
+		else
+			redirect_to 'basic_pages/home'
+		end
+	end
+
+	def destroy
+		@task.destroy
+		redirect_to request.referrer || root_url
+	end
+
+	private
+		def task_params
+			params.require(:task).permit(:content)
+		end
 end
